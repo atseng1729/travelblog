4b03ef5521ae15cdb8edc70de35834405b0cbd33
atseng1729 <atseng1729@gmail.com>
2020-01-02 21:33:58 -0500

    first draft of travel webpage

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Travel Photography
-Static webpage for my travel blog
+# Travel Photography Webpage
+A repository that generates the static webpage https://atseng1729.github.io/gallery.
+
+# Setup
+To run locally do the following:
+- Clone the repository through terminal by running
+`git clone https://github.com/atseng1729/gallery.git`
+- Follow ![this](https://help.github.com/en/github/working-with-github-pages/testing-your-github-pages-site-locally-with-jekyll) link to install Ruby and Jekyll
+- Create the `config.json` file by running `python tools/setup.py`
+- Build and host your website by running `jekyll server`
+- Open `localhost:4000` to view the website
---
+++ tools/setup.py
@@ -0,0 +1,104 @@
+try:
+    from StringIO import StringIO as sbIO
+except ImportError:
+    from io import BytesIO as sbIO
+import json
+import os
+import struct
+
+
+PATH = os.path.dirname(__file__) + '/../'
+RELATIVE_PATH = 'photos'
+PHOTO_PATH = PATH + RELATIVE_PATH
+
+
+def getImageInfo(data):
+    size = len(data)
+    height = -1
+    width = -1
+    content_type = ''
+
+    # See PNG 2. Edition spec (http://www.w3.org/TR/PNG/)
+    # Bytes 0-7 are below, 4-byte chunk length, then 'IHDR'
+    # and finally the 4-byte width, height
+    if ((size >= 24) and data.startswith(b'\211PNG\r\n\032\n') and
+       (data[12:16] == b'IHDR')):
+        content_type = 'image/png'
+        w, h = struct.unpack(">LL", data[16:24])
+        width = int(w)
+        height = int(h)
+
+    # Maybe this is for an older PNG version.
+    elif (size >= 16) and data.startswith(b'\211PNG\r\n\032\n'):
+        # Check to see if we have the right content type
+        content_type = 'image/png'
+        w, h = struct.unpack(">LL", data[8:16])
+        width = int(w)
+        height = int(h)
+
+    # handle JPEGs
+    elif (size >= 2) and data.startswith(b'\xff\xd8'):
+        content_type = 'image/jpeg'
+
+        try:
+            jpeg = sbIO(data)  # Python 3
+        except:
+            jpeg = sbIO(str(data))  # Python 2
+
+        jpeg.read(2)
+        b = jpeg.read(1)
+        try:
+            while (b and ord(b) != 0xDA):
+                while (ord(b) != 0xFF):
+                    b = jpeg.read(1)
+                while (ord(b) == 0xFF):
+                    b = jpeg.read(1)
+                if (ord(b) >= 0xC0 and ord(b) <= 0xC3):
+                    jpeg.read(3)
+                    h, w = struct.unpack(">HH", jpeg.read(4))
+                    break
+                else:
+                    jpeg.read(int(struct.unpack(">H", jpeg.read(2))[0])-2)
+                b = jpeg.read(1)
+            width = int(w)
+            height = int(h)
+        except struct.error:
+            pass
+        except ValueError:
+            pass
+
+    return content_type, width, height
+
+
+def get_directories():
+    items = os.listdir(PHOTO_PATH)
+    return list(filter(lambda x: os.path.isdir(PHOTO_PATH + '/' + x), items))
+
+
+def get_images(path):
+    images = list(sorted(os.listdir(PHOTO_PATH + '/' + path)))
+
+    result = []
+    for img in images:
+        temp_path = './' + RELATIVE_PATH + '/' + path + '/' + img
+        with open(temp_path, 'rb') as f:
+            _, width, height = getImageInfo(f.read())
+        result.append({
+            'width': width,
+            'height': height,
+            'path': './' + RELATIVE_PATH + '/' + path + '/' + img
+        })
+    return result
+
+
+def write_config(config):
+    with open(PATH + 'config.json', 'w') as f:
+        f.write(json.dumps(config, indent=2, separators=(',', ': ')))
+
+
+if __name__=="__main__":
+    config = {}
+    directories = get_directories()
+    for path in directories:
+        config[path] = get_images(path)
+    write_config(config)
